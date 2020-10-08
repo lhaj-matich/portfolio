742f381ad50230dadc5cf6d7be49e776c85cc373
Ochoumoul <pentestossa@gmail.com>
2020-10-08 16:39:04 +0100

    Updates

---
+++ precache-manifest.bdc74cac0691d1576cae934ac1fbdc55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c414c6e4528b02db83c06b5e3949c37",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e73dda487de4f593b83e",
+    "url": "/portfolio/static/css/main.d8219422.chunk.css"
+  },
+  {
+    "revision": "ddfe326aace2bf74198a",
+    "url": "/portfolio/static/js/2.c3720237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c3720237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73dda487de4f593b83e",
+    "url": "/portfolio/static/js/main.0c1b717a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
